7655116694b7df5a672c7e0b9e6323e6431e8a8e
Johannes Seitz <johannes.w.seitz@gmail.com>
2013-12-14 14:14:19 +0100

    Change comment

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -1,4 +1,4 @@
-# Inspired by http://sleepycoders.blogspot.de/2013/03/sharing-travis-ci-generated-files.html?_escaped_fragment_#!
+# Take from and modified http://sleepycoders.blogspot.de/2013/03/sharing-travis-ci-generated-files.html?_escaped_fragment_#!
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
 
