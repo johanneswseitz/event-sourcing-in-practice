67952b5c714cce88be54c4fc84d574728282bfdf
Johannes Seitz <johannes.w.seitz@gmail.com>
2013-12-14 15:10:31 +0100

    Don't copy the temporary directory but only its content

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -17,7 +17,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
-  cp -Rf $TEMP_DIR .
+  cp -Rf $TEMP_DIR/* .
 
   #add, commit and push files
   git add -f .
