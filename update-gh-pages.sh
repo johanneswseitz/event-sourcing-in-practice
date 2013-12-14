7d87f290869aac794d55246b3d714cb081678508
Johannes Seitz <johannes.seitz@vaamo.de>
2013-12-14 14:07:55 +0100

    Don't suppress errors

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Ookami86/event-sourcing-in-practice.git  gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Ookami86/event-sourcing-in-practice.git  gh-pages
 
   #go into diractory and copy data we're interested in to that directory
   cd gh-pages
@@ -20,7 +20,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   #add, commit and push files
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages 
 
   echo -e "Done publishing to gh-pages.\n"
 fi
